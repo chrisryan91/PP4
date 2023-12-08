f267a7fd4e9710d64213606d0c52261ddba05484
Chris <christopherryan91@gmail.com>
2023-12-08 23:49:00 +0000

    Restyle review.html by removing last div and add styling for image by placing it in new div

--- pp4app/urls.py
+++ pp4app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Reviews.as_view(), name="home"),
-    path('<slug:slug>', views.ReviewPost.as_view(), name='review_post'),
+    path('<slug:slug>/', views.ReviewPost.as_view(), name='review_post'),
 ]
