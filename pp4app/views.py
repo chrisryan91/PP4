66153d4bc5dbc0f078bd2a2636b4d73e6c4ccff3
Chris <christopherryan91@gmail.com>
2023-12-10 17:48:21 +0000

    Add functionality to Search view to handle API requess with a function based view, include error handling, return the data to search html, add div containers to search html to render returned data

--- pp4app/views.py
+++ pp4app/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Review
+from decouple import config
+import requests
+import os
 
 def Homepage(request):
     return render(request, 'index.html')
 
 def Search(request):
-    return render(request, 'search.html')
+    if request.method == 'POST':
+        query = request.POST.get('query')
+
+        api_url = 'https://api.edamam.com/api/recipes/v2'
+        app_id = os.environ.get("EDA_APP_ID")
+        app_key = os.environ.get("EDA_APP_KEY")
+
+        params = {
+            'q': query,
+            'app_id': app_id,
+            'app_key': app_key,
+            'type': 'public',
+        }
+
+        response = requests.get(api_url, params=params)
+        
+        if response.status_code == 200:
+            data = response.json()
+            recipes = data.get('hits', [])
+            return render(request, 'search.html', {'query': query, 'recipes': recipes})
+        else:
+            error_message = 'Error fetching recipes from the API.'
+            return render(request, 'error.html', {'error_message': error_message})
+
+    return render(request, 'search.html', {'query': '', 'recipes': []})
 
 def SubmitReview(request):
     return render(request, 'submit_review.html')
